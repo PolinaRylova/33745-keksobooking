b364fe92383640dce78e21e161a7f5b0ce9542ba
Polina Rylova <miracleserenity@gmail.com>
2017-09-06 22:16:59 +0300

    Поправила пути скриптов в html, изменила стили для сообщения об ошибке в модуле backend.js и реализовала загрузку данных объявлений с сервера

--- js/backend.js
+++ js/backend.js
@@ -34,7 +34,7 @@
     error: function (message) {
       var errorBlock = document.createElement('div');
       errorBlock.style.width = 50 + '%';
-      errorBlock.style.height = 120 + 'px';
+      errorBlock.style.height = 50 + 'px';
       errorBlock.style.position = 'fixed';
       errorBlock.style.top = 50 + '%';
       errorBlock.style.left = 50 + '%';
@@ -43,7 +43,10 @@
       errorBlock.style.display = 'flex';
       errorBlock.style.alignItems = 'center';
       errorBlock.style.justifyContent = 'center';
-      errorBlock.style.border = '3 px solid red';
+      errorBlock.style.backgroundColor = '#ffffff';
+      errorBlock.style.border = '3px solid #ff5635';
+      errorBlock.style.borderRadius = 50 + '%';
+      errorBlock.style.color = '#ff5635';
       errorBlock.textContent = message;
       document.body.insertAdjacentElement('afterbegin', errorBlock);
     }
