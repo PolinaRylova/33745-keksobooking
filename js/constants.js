f0b5e6e9cb3ff0f806a43c0cca30bdb54de1aef1
Polina Rylova <mail@polinarya.ru>
2017-09-18 17:09:09 +0500

    Вынесла проверку нажатой клавиши в отдельный модуль

--- js/constants.js
+++ js/constants.js
@@ -3,8 +3,6 @@
   window.constants = {
     SERVER_URL: 'https://1510.dump.academy/keksobooking',
     DEBOUNCE_INTERVAL: 500, // ms
-    ENTER_KEY: 13,
-    ESCAPE_KEY: 27,
     ERROR_RED_SHADOW: '0 0 5px 2px red',
     ADV_COUNT: 3,
     FIELD_MIN_LENGTH: 30
